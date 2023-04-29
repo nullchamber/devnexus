6d5001a72378e3072dc88ded3fcfccefb8d1ece2
Dylan Garrett <dreadnexus@protonmail.ch>
2023-04-29 09:10:01 +0000

    Site iniitialized

--- dev_nexus/dev_nexus/settings.py
+++ dev_nexus/dev_nexus/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-$3xjjk3+%0wn$(uln7%75!7v^@s_zbo_vowyq77uiu7yey@+cr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '161.35.254.167'
+]
 
 
 # Application definition
